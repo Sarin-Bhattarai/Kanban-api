f1329ae152d505962cbb4e2d220bb5f26b387ecc
Sarin Bhattarai <sarin.bhattarai.a19@icp.edu.np>
2024-11-25 18:50:22 +0545

    feat(controllers)

---
+++ controllers/user.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+module.exports = {
+  registerUser: async (req, res) => {
+    try {
+      const checkUser = await User.findOne({ email: req.body.email });
+      if (checkUser) {
+        return res.status(400).json({
+          status: "error",
+          message: "Email already in use",
+        });
+      }
+
+      const newUser = new User(req.body);
+      const result = await newUser.save();
+      const { password, ...user } = result.toObject(); // De-structuring to avoid sending hashed password
+
+      return res.status(200).json({
+        status: "success",
+        data: { user },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+  },
+
+  loginUser: async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        return res.status(404).json({
+          status: "error",
+          message: "User doesn't exist",
+        });
+      }
+
+      const passwordMatch = await user.matchPassword(req.body.password); // Use matchPassword from the model
+      if (!passwordMatch) {
+        return res.status(400).json({
+          status: "error",
+          message: "Invalid password",
+        });
+      }
+
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_PRIVATE_KEY);
+      return res.header("x-auth-token", token).json({ token });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+  },
+};
